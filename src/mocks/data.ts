4b2313c0b34b5600493a432f39b138c21eb57ddb
Kiril Varabei <kiril_varabei@epam.com>
2023-10-09 19:22:44 +0200

    Add support for new products api

--- src/mocks/data.ts
+++ src/mocks/data.ts
@@ -8,7 +8,7 @@ export const products: Product[] = [
     description: "Short Product Description1",
     id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
     price: 24,
-    title: "ProductOne",
+    title: "1",
   },
   {
     description: "Short Product Description7",

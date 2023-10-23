656aa50343a08a6a7da2db18376e3549076f35de
Kiril Varabei <kiril_varabei@epam.com>
2023-10-23 22:48:26 +0200

    feat: change import api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://4e25j8emej.execute-api.eu-central-1.amazonaws.com",
   bff: "https://jba668sxc7.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
